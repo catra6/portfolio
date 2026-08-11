6f671c7d415bebb0b4cfa5c969396368bf593c22
facundo prosiuk <prosiuk@proton.me>
2026-08-11 19:27:24 -0300

    Add files via upload

---
+++ xmb-wave/ps3xmbwave/particles-settings.js
@@ -0,0 +1,16 @@
+'use strict';
+window.PARTICLE_SETTINGS = {
+  "count": 978,
+  "opacity": 0.62,
+  "sizeBase": 2.6,
+  "sizeVar": 1.5,
+  "flowSpeed": 0.18
+};
+
+window.PARTICLE_SETTINGS_META = {
+  count: { min: 10, max: 4000, step: 1, decimals: 0 },
+  opacity: { min: 0, max: 1, step: 0.01 },
+  sizeBase: { min: 1, max: 40, step: 0.1 },
+  sizeVar: { min: 0, max: 50, step: 0.1 },
+  flowSpeed: { min: 0, max: 3, step: 0.01 },
+};
